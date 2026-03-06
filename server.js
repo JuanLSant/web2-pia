f339044775b8530b08f7be480e1c52abcd615c5a
AlejandraTar <vaaleriiaagaarzzaa@gmail.com>
2026-03-06 15:10:53 -0600

    Ajustando conexion
    Existía error en server.js que bloqueaba la comunicacion con el Node

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -17,10 +18,13 @@ const db = mysql.createConnection({
 });
 
 app.post('/login', (req, res) => {
+    console.log("Datos recibidos:", req.body);
+
+
     const { username, password } = req.body;
     const sql = "SELECT * FROM usuarios WHERE nombre = ? AND password = ?";
     db.query(sql, [username, password], (err, data) => {
-        if(err) return res.json(err);
+        if(err) return res.status(500).json(err);
         if(data.length > 0) return res.json("Success");
         return res.status(401).json({ alerta: "Usuario o contraseña no coinciden" });
     })
